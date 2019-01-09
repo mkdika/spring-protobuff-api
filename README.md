5bd4cebff96040174df8521d6024626770d60d61
Maikel Chandika <mkdika@gmail.com>
2019-01-09 21:56:22 +0700

    First init.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spring Protocol Buff API Example
+
+Spring Boot and Google protocol buffer API example.
+
+
+
+### Reference
+
+https://www.baeldung.com/spring-rest-api-with-protocol-buffers
---
+++ src/main/kotlin/com/mkdika/springprotobuffapi/SpringprotobuffapiApplication.kt
@@ -0,0 +1,12 @@
+package com.mkdika.springprotobuffapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringprotobuffapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringprotobuffapiApplication>(*args)
+}
+
